aacf168b9cc10b3c4ac0f6f0dcd3c3a0a5507920
jenmyers <jmyers@gmail.com>
2013-09-14 15:56:43 -0500

    Add initial app files.

---
+++ app.rb
@@ -0,0 +1,17 @@
+require "sinatra"
+require "kickscraper"
+
+Kickscraper.configure do |config|
+	config.email = ENV["KICKSTARTER_EMAIL"]
+	config.password = ENV["KICKSTARTER_PASSWORD"]
+end
+
+get "/" do
+	erb :index
+end
+
+get "/project" do
+	client = Kickscraper.client
+	@project = client.category("Narrative Film").projects.sample
+	erb :project
+end
---
+++ config.rb
@@ -0,0 +1,30 @@
+require "susy"
+
+# Configuration to use when running within Sinatra
+#project_path          = Sinatra::Application.root
+
+# HTTP paths
+http_path             = '/'
+http_stylesheets_path = '/stylesheets'
+http_images_path      = '/images'
+http_javascripts_path = '/javascripts'
+http_fonts_path 			= '/stylesheets/fonts'
+
+# File system locations
+css_dir               = File.join 'public', 'stylesheets'
+sass_dir              = File.join 'sass'
+images_dir            = File.join 'public', 'images'
+javascripts_dir       = File.join 'public', 'javascripts'
+fonts_path						= File.join 'public', 'stylesheets', 'fonts'
+
+# Syntax preference
+preferred_syntax      = :scss
+
+# Determine whether Compass generates relative or absolute paths
+relative_assets       = false
+
+# Determines whether line comments should be added to compiled css for easier debugging
+line_comments         = false
+
+# CSS output style - :nested, :expanded, :compact, or :compressed
+output_style          = :expanded
